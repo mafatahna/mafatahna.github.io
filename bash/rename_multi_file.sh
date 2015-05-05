85fe08095835dfb916965250835123cee9df3a5d
mafatahna <mafatahna@gmail.com>
2015-05-05 14:09:25 +0700

    add

--- bash/rename_multi_file.sh
+++ bash/rename_multi_file.sh
@@ -1,5 +1,5 @@
 # Rename file from "Scorpion Season 1 Episode *.mp4" to "Scorpion Season 1"
-
+# 
 for file in  Scorpion\ Season\ 1\ Episode\ *.mp4
 do
         mv -i "${file}" "${file/Scorpion\ Season\ 1\ Episode/Scorpion\ Season\ 1}"
